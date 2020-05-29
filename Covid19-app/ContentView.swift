9084417d090d1fd4ec5ed003b02191a552556b2f
ajpicard913 <Contact@saucedesigns.io>
2020-05-29 11:31:13 -0400

    Initial Commit

---
+++ Covid19-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19-app
+//
+//  Created by AJ Picard on 5/29/20.
+//  Copyright © 2020 AJ Picard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
